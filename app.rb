a3811bd06e428f309b692ca029b62c978ac3f98e
Gor <7495500@gmail.com>
2020-03-14 22:39:05 +0300

    Save content to database

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+#Create database
+
 def init_db
   @db = SQLite3::Database.new 'leprosorium.db'
   @db.results_as_hash = true
@@ -10,6 +12,20 @@ end
 
 before do
   init_db
+end
+
+#Create table
+
+configure do
+
+  init_db
+
+  db.execute 'CREATE TABLE "leprosorium"
+   (
+     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+     "created_date" DATE,
+     "column3" TEXT
+     );'
 end
 
 get '/' do
@@ -21,9 +37,14 @@ get '/new' do
 end
 
 post '/new' do
-
-
   content = params[:content]
+
+  if content.length <= 0
+    @error = 'Type post text'
+    return erb :new
+  end
+
+  @db.execute "insert into Posts (content, created_date) values (?, datetime())", [content]
 
   erb "You typed: #{content}"
 end
