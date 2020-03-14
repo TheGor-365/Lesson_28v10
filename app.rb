2e201827b1f5aed49f6112ee34094b357b9ac185
Gor <7495500@gmail.com>
2020-03-15 00:01:52 +0300

    Save comment to database

--- app.rb
+++ app.rb
@@ -74,5 +74,10 @@ post '/details/:post_id' do
 
   content = params[:content]
 
-  erb "You tiped comment #{comment} for post #{post_id}"
+  @db.execute "insert into Comments
+    (content, created_date, post_id)
+    values (?, datetime(), ?
+  )", [content, post_id]
+
+  redirect to('/details/ + post_id')
 end
